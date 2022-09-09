07eb389032e0eef7707e766b5be5d3fb92f83555
KrHo <krhodev@gmail.com>
2022-09-09 23:09:30 +0200

    application.properties - Spring boot

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -7,7 +7,6 @@ import guru.springframework.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
@@ -21,7 +20,6 @@ public class GreetingServiceConfig {
         fakeDataSource.setUsername(username);
         fakeDataSource.setPassword(password);
         fakeDataSource.setJsdcurl(jdbcurl);
-
 
         return fakeDataSource;
     }
